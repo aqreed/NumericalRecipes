a83cbeb4642c64e3122e90b11fe2f18c18507176
aqreed <andres.quezada.reed@gmail.com>
2019-03-21 22:14:35 +0100

    [FORTRAN] test for Log Gamma function simplified even more

--- test/fortran_libs/lib_stat/test_gammaln.py
+++ test/fortran_libs/lib_stat/test_gammaln.py
@@ -17,20 +17,24 @@ from subprocess import check_output
 from numpy.testing import assert_almost_equal
 
 
+def run_fortran_gammaln(x):
+    return float((check_output(['./test_gammaln',
+                 str(x)])).decode("utf-8").split('\n')[0])
+
+
 def test_gammaln():
-    c1 = float((check_output(['./test_gammaln',
-                str(0.01)])).decode("utf-8").split('\n')[0])
+    """
+        Checks four values >
+    """
+    x1, x2, x3, x4 = 0.01, 1.0, 2.0, 5.0
 
-    c2 = float((check_output(['./test_gammaln',
-                str(1.0)])).decode("utf-8").split('\n')[0])
-
-    c3 = float((check_output(['./test_gammaln',
-                str(2.0)])).decode("utf-8").split('\n')[0])
-
-    c4 = float((check_output(['./test_gammaln',
-                str(5.0)])).decode("utf-8").split('\n')[0])
+    c1 = run_fortran_gammaln(x1)
+    c2 = run_fortran_gammaln(x2)
+    c3 = run_fortran_gammaln(x3)
+    c4 = run_fortran_gammaln(x4)
 
     calculated_value = [c1, c2, c3, c4]
-    expected_value = [4.599479878042021722514, 0, 0, 3.178053830347945619647]
+    expected_value = [4.599479878042021722514, 0,
+                      0, 3.178053830347945619647]
 
     assert_almost_equal(calculated_value, expected_value)
