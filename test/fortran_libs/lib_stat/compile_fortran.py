3466407e8fa22019bb869f63857e95867bd73354
aqreed <andres.quezada.reed@gmail.com>
2019-04-12 23:43:01 +0200

    added header to compilation script

--- test/fortran_libs/lib_stat/compile_fortran.py
+++ test/fortran_libs/lib_stat/compile_fortran.py
@@ -1,3 +1,9 @@
+"""
+    Script to compile the fortran files required to serve as
+    benchmark in the Julia and Python notebooks.
+"""
+
+
 from os import system
 
 
@@ -8,7 +14,7 @@ system('gfortran -c ' + path + '/lib_stat.f90')
 # Compile the fortran tests source code to object files
 system('gfortran -c *.f90')
 
-# Link the objects to create standalone programs that 
+# Link the objects to create standalone programs that
 # later will be called from the python scripts
 system('gfortran lib_stat.o test_avevar.o -o fmain_test_avevar')
 system('gfortran lib_stat.o test_betai.o -o fmain_test_betai')
