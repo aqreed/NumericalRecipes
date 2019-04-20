fcaca1dbd82a4767a8339c16dfcca927dc66ddc3
aqreed <andres.quezada.reed@gmail.com>
2019-04-20 23:33:31 +0200

    Python script that compile Fortran code split in several others. Original one deleted

---
+++ src/Ch13_StatisticalDescriptionOfData/student_ttest/paired_samples/compile_fortran.py
@@ -0,0 +1,30 @@
+"""
+    Script to compile the Fortran files required to serve as
+    benchmark in the Julia and Python notebooks.
+"""
+
+
+from os import system, path, getcwd, chdir
+
+
+# get current directory and change dir to it
+chdir(getcwd())
+
+# Compile Fortran source code to object file
+system('gfortran -c ../../../Ch6_SpecialFunctions/betacf/betacf.f90')
+system('gfortran -c ../../../Ch6_SpecialFunctions/gammaln/gammaln.f90')
+system('gfortran -c ../../../Ch6_SpecialFunctions/betai/betai.f90')
+system('gfortran -c ../../avevar/avevar.f90')
+
+# Compile the Fortran program and speed test source codes to object files,
+# then link the objects to create standalone programs
+if not path.isfile('fmain_tptest') or not path.isfile('fspeed_tptest'):
+    # compile
+    system('gfortran -c tptest.f90')
+    system('gfortran -c tptest_speedTest.f90')
+    # link
+    system('gfortran betai.o betacf.o gammaln.o avevar.o tptest.o -o fmain_tptest')
+    system('gfortran betai.o betacf.o gammaln.o avevar.o tptest_speedTest.o -o fspeed_tptest')
+
+# Remove unnecessary objects and module
+system('rm *.o *.mod')
---
+++ src/Ch13_StatisticalDescriptionOfData/student_ttest/significantly_different_means/compile_fortran.py
@@ -0,0 +1,30 @@
+"""
+    Script to compile the Fortran files required to serve as
+    benchmark in the Julia and Python notebooks.
+"""
+
+
+from os import system, path, getcwd, chdir
+
+
+# get current directory and change dir to it
+chdir(getcwd())
+
+# Compile Fortran source code to object file
+system('gfortran -c ../../../Ch6_SpecialFunctions/betacf/betacf.f90')
+system('gfortran -c ../../../Ch6_SpecialFunctions/gammaln/gammaln.f90')
+system('gfortran -c ../../../Ch6_SpecialFunctions/betai/betai.f90')
+system('gfortran -c ../../avevar/avevar.f90')
+
+# Compile the Fortran program and speed test source codes to object files,
+# then link the objects to create standalone programs
+if not path.isfile('fmain_ttest') or not path.isfile('fspeed_ttest'):
+    # compile
+    system('gfortran -c ttest.f90')
+    system('gfortran -c ttest_speedTest.f90')
+    # link
+    system('gfortran betai.o betacf.o gammaln.o avevar.o ttest.o -o fmain_ttest')
+    system('gfortran betai.o betacf.o gammaln.o avevar.o ttest_speedTest.o -o fspeed_ttest')
+
+# Remove unnecessary objects and module
+system('rm *.o *.mod')
