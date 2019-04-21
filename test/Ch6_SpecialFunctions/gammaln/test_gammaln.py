544e4225316a7d7c621841cdbc57479fd8ad6fe5
aqreed <andres.quezada.reed@gmail.com>
2019-04-21 17:14:08 +0200

    [PYTHON] added test for gammaln. Created own directory

---
+++ test/Ch6_SpecialFunctions/gammaln/test_gammaln.py
@@ -0,0 +1,56 @@
+"""
+    Unit tests of the Log Gamma function
+
+    Expected values extracted from:
+        [URL] https://keisan.casio.com/exec/system/1180573442
+"""
+
+
+import pytest
+import os
+import sys
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+sys.path.append(os.path.realpath('../../../src/Ch6_SpecialFunctions/gammaln'))
+from gammaln import gammaln
+
+
+def test_gammaln():
+    """
+        Checks values x > 0
+    """
+    x1, x2, x3, x4 = 0.01, 1.0, 2.0, 5.0
+
+    c1 = gammaln(x1)
+    c2 = gammaln(x2)
+    c3 = gammaln(x3)
+    c4 = gammaln(x4)
+
+    calculated_value = [c1, c2, c3, c4]
+    expected_value = [4.599479878042021722514, 0,
+                      0, 3.178053830347945619647]
+
+    assert_almost_equal(calculated_value, expected_value)
+
+
+def test_gammaln_recurrence():
+    """
+        Gamma log function satisfies the recurrence relation:
+            gamma(z+1) = z * gamma(z)
+
+        Accordingly, this test checks that:
+            gammaln(z+1) = ln(z) + gammaln(z)
+    """
+
+    x1, x2, x3, x4 = 0.001, 1.1, 3.32, 23.11
+
+    c1 = gammaln(x1 + 1) - (np.log(x1) + gammaln(x1))
+    c2 = gammaln(x2 + 1) - (np.log(x2) + gammaln(x2))
+    c3 = gammaln(x3 + 1) - (np.log(x3) + gammaln(x3))
+    c4 = gammaln(x4 + 1) - (np.log(x4) + gammaln(x4))
+
+    calculated_value = [c1, c2, c3, c4]
+    expected_value = [0, 0, 0, 0]
+
+    assert_almost_equal(calculated_value, expected_value)
