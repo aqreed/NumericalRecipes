7f7ab091c55fd34e2645a34689f233faa1529268
aqreed <andres.quezada.reed@gmail.com>
2019-04-21 22:14:39 +0200

    [JULIA] added test for avevar

---
+++ test/Ch13_StatisticalDescriptionOfData/avevar/test_avevar.jl
@@ -0,0 +1,82 @@
+#=
+    Unit tests of the avevar function
+
+    Expected values extracted from:
+        [URL] http://www.alcula.com/calculators/statistics/variance/
+=#
+
+
+include("../../../src/Ch13_StatisticalDescriptionOfData/avevar/avevar.jl")
+using .mod_avevar: avevar
+
+
+function test_avevar_10()
+    #=
+        Checks average values of an input dataset
+    =#
+    data = [1, 1, 1, 1, 1]
+    calculated_value = avevar(data)[1]
+    expected_value = 1.0
+
+    return isapprox(calculated_value, expected_value; atol=1e-8)
+end
+
+
+function test_avevar_11()
+    #=
+        Checks average values of an input dataset
+    =#
+    data = [-2, -1, 0, 1, 2]
+    calculated_value = avevar(data)[1]
+    expected_value = 0.0
+
+    return isapprox(calculated_value, expected_value; atol=1e-8)
+end
+
+
+function test_avevar_12()
+    #=
+        Checks average values of an input dataset
+    =#
+    data = [1, 2, 3, 4, 5]
+    calculated_value = avevar(data)[1]
+    expected_value = 3
+
+    return isapprox(calculated_value, expected_value; atol=1e-8)
+end
+
+
+function test_avevar_20()
+    #=
+        Checks variance (sample) values of an input dataset
+    =#
+    data = [1, 1, 1, 1, 1]
+    calculated_value = avevar(data)[2]
+    expected_value = 0.0
+
+    return isapprox(calculated_value, expected_value; atol=1e-8)
+end
+
+
+function test_avevar_21()
+    #=
+        Checks variance (sample) values of an input dataset
+    =#
+    data = [1, 2, 3, 4, 5]
+    calculated_value = avevar(data)[2]
+    expected_value = 2.5
+
+    return isapprox(calculated_value, expected_value; atol=1e-8)
+end
+
+
+function test_avevar_22()
+    #=
+        Checks variance (sample) values of an input dataset
+    =#
+    data = [1, 3, 5, 7, 9]
+    calculated_value = avevar(data)[2]
+    expected_value = 10.0
+
+    return isapprox(calculated_value, expected_value; atol=1e-8)
+end
