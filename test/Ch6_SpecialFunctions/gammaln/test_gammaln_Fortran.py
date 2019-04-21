0554fdaf9873283c550b28ccd5e0d3c085e167df
aqreed <andres.quezada.reed@gmail.com>
2019-04-21 17:57:31 +0200

    [FORTRAN] added test for gammaln. Modified gitignore

---
+++ test/Ch6_SpecialFunctions/gammaln/test_gammaln_Fortran.py
@@ -0,0 +1,71 @@
+"""
+    Unit tests of the Log Gamma function
+
+    Expected values extracted from:
+        [URL] https://keisan.casio.com/exec/system/1180573442
+
+    Requires previous compilation of Fortran source code:
+
+        $ gfortran -c gammaln.f90 test_gammaln.f90
+        $ gfortran gammaln.o test_gammaln.o -o ftest_gammaln
+"""
+
+
+import pytest
+import numpy as np
+from os import system
+from subprocess import check_output
+from numpy.testing import assert_almost_equal
+
+
+# Compile the Fortran tests source code to object files
+path = '../../../src/Ch6_SpecialFunctions/gammaln'
+system('gfortran -c ' + path + '/gammaln.f90 test_gammaln.f90')
+system('gfortran gammaln.o test_gammaln.o -o ftest_gammaln')  # Link objects
+system('rm *.o *.mod')  # Remove unnecessary objects and module
+
+
+# Fortran interface function
+def fortran_gammaln(x):
+    return float((check_output(['./ftest_gammaln',
+                 str(x)])).decode("utf-8").split('\n')[0])
+
+
+def test_gammaln():
+    """
+        Checks values x > 0
+    """
+    x1, x2, x3, x4 = 0.01, 1.0, 2.0, 5.0
+
+    c1 = fortran_gammaln(x1)
+    c2 = fortran_gammaln(x2)
+    c3 = fortran_gammaln(x3)
+    c4 = fortran_gammaln(x4)
+
+    calculated_value = [c1, c2, c3, c4]
+    expected_value = [4.599479878042021722514, 0,
+                      0, 3.178053830347945619647]
+
+    assert_almost_equal(calculated_value, expected_value)
+
+
+def test_gammaln_recurrence():
+    """
+        Gamma log function satisfies the recurrence relation:
+            gamma(z+1) = z * gamma(z)
+
+        Accordingly, this test checks that:
+            gammaln(z+1) = ln(z) + gammaln(z)
+    """
+
+    x1, x2, x3, x4 = 0.001, 1.1, 3.32, 23.11
+
+    c1 = fortran_gammaln(x1 + 1) - (np.log(x1) + fortran_gammaln(x1))
+    c2 = fortran_gammaln(x2 + 1) - (np.log(x2) + fortran_gammaln(x2))
+    c3 = fortran_gammaln(x3 + 1) - (np.log(x3) + fortran_gammaln(x3))
+    c4 = fortran_gammaln(x4 + 1) - (np.log(x4) + fortran_gammaln(x4))
+
+    calculated_value = [c1, c2, c3, c4]
+    expected_value = [0, 0, 0, 0]
+
+    assert_almost_equal(calculated_value, expected_value)
