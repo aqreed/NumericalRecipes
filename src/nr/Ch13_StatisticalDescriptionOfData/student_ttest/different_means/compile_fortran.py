69808e4e5dd024ab076430036e33f59f54629772
aqreed <andres.quezada.reed@gmail.com>
2019-04-29 00:02:01 +0200

    [FORTRAN] edited Python scripts removing speedTests

--- src/nr/Ch13_StatisticalDescriptionOfData/student_ttest/different_means/compile_fortran.py
+++ src/nr/Ch13_StatisticalDescriptionOfData/student_ttest/different_means/compile_fortran.py
@@ -1,27 +1,25 @@
 """
-    Script to compile the Fortran files required to serve as
-    benchmark in the Julia and Python notebooks.
+    Script to compile the Fortran files required to serve
+    as benchmark in the Julia and Python notebooks.
 """
 
 
 from os import system, path
 
 
-# Compile Fortran source code to object file
+# Compile modules Fortran source code to object files
 system('gfortran -c ../../../Ch6_SpecialFunctions/betacf/betacf.f90')
 system('gfortran -c ../../../Ch6_SpecialFunctions/gammaln/gammaln.f90')
 system('gfortran -c ../../../Ch6_SpecialFunctions/betai/betai.f90')
 system('gfortran -c ../../avevar/avevar.f90')
 
-# Compile the Fortran program and speed test source codes to object files,
-# then link the objects to create standalone programs
-if not path.isfile('fmain_ttest') or not path.isfile('fspeed_ttest'):
+# Compile main Fortran program source code and then
+# link the objects to create the executable
+if not path.isfile('fmain_ttest'):
     # compile
     system('gfortran -c ttest.f90')
-    system('gfortran -c ttest_speedTest.f90')
     # link
     system('gfortran betai.o betacf.o gammaln.o avevar.o ttest.o -o fmain_ttest')
-    system('gfortran betai.o betacf.o gammaln.o avevar.o ttest_speedTest.o -o fspeed_ttest')
 
 # Remove unnecessary objects and module
 system('rm *.o *.mod')
