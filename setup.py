f5a3560c29835599cf46d844943bea526c02b42e
aqreed <andres.quezada.reed@gmail.com>
2019-10-17 12:19:24 +0200

    [PYTHON] improved and fixed setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='numericalrecipes',
     description='Numerical Recipes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='aqreed',
-    version='0.1.0',
-    packages=find_packages('src'),
+    version='0.0.1',
+    url='https://github.com/aqreed/NumericalRecipes',
     package_dir={'': 'src'},
-    test_suite='test'
+    install_requires=['numpy', 'matplotlib'],
+    tests_requires=['pytest']
 )
