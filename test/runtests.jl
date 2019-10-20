dbcf92d0d9db373898cb4161ba9d607318b8e504
aqreed <andres.quezada.reed@gmail.com>
2019-10-21 01:21:17 +0200

    [JULIA] added test for "ttest" function. Added HypothesisTests package to deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,4 @@ using NumericalRecipes
 @testset "test_betai" begin include("Ch6_SpecialFunctions/betai/test_betai.jl") end
 @testset "test_gamma" begin include("Ch6_SpecialFunctions/gammaln/test_gammaln.jl") end
 @testset "test_avevar" begin include("Ch13_StatisticalDescriptionOfData/avevar/test_avevar.jl") end
+@testset "test_ttest_means" begin include("Ch13_StatisticalDescriptionOfData/student_ttest/different_means/test_ttest_means.jl") end
