f5fec3add139b0bc29673258e8090bb4724481ff
aqreed <andres.quezada.reed@gmail.com>
2019-10-29 16:48:29 +0100

    [PYTHON][FORTRAN] renamed "ttest" test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,4 @@ using NumericalRecipes
 @testset "test_betai" begin include("Ch6_SpecialFunctions/betai/test_betai.jl") end
 @testset "test_gammaln" begin include("Ch6_SpecialFunctions/gammaln/test_gammaln.jl") end
 @testset "test_avevar" begin include("Ch13_StatisticalDescriptionOfData/avevar/test_avevar.jl") end
-@testset "test_ttest_means" begin include("Ch13_StatisticalDescriptionOfData/student_ttest/different_means/test_ttest_means.jl") end
+@testset "test_ttest" begin include("Ch13_StatisticalDescriptionOfData/student_ttest/different_means/test_ttest.jl") end
