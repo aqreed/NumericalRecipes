55cf4e2a09e5990a18953b875b72545c89165399
aqreed <andres.quezada.reed@gmail.com>
2019-11-04 23:27:50 +0100

    [FORTRAN] added test for tutest function. Modified tutest file to contain only the subroutine

--- src/numericalrecipes/Ch13_StatisticalDescriptionOfData/student_ttest/different_variances/tutest.f90
+++ src/numericalrecipes/Ch13_StatisticalDescriptionOfData/student_ttest/different_variances/tutest.f90
@@ -1,64 +1,32 @@
-subroutine tutest(data1, n1, data2, n2, t, prob)
-   ! Given the arrays "data1" and "data2" of lengths "n1" and "n2",
-   ! returns the Student's "t" and its significance as "prob".
-   ! Data are assumed to be drawn from populations with unequal
-   ! variances.
-   ! Small values of "prob" indicates that the arrays have different means.
-   
-   use mod_betai
-   use mod_avevar
+module mod_tutest
+   ! Contains the following subroutine: tutest
    implicit none
-   real(8), intent(in)                  :: data1(n1), data2(n2)
-   integer, intent(in)                  :: n1, n2
-   real(8)                              :: ave1, var1, ave2, var2, &
-                                           df, var, t, prob
+   contains
+
+   subroutine tutest(data1, n1, data2, n2, t, prob)
+      ! Given the arrays "data1" and "data2" of lengths "n1" and "n2",
+      ! returns the Student's "t" and its significance as "prob".
+      ! Data are assumed to be drawn from populations with unequal
+      ! variances.
+      ! Small values of "prob" indicates that the arrays have different means.
+      use mod_betai
+      use mod_avevar
+      implicit none
+      real(8), intent(in)                  :: data1(n1), data2(n2)
+      integer, intent(in)                  :: n1, n2
+      real(8)                              :: ave1, var1, ave2, var2, &
+                                              df, var, t, prob
 
 
-   call avevar(data1, n1, ave1, var1)
-   call avevar(data2, n2, ave2, var2)
+      call avevar(data1, n1, ave1, var1)
+      call avevar(data2, n2, ave2, var2)
 
-   ! Student's t
-   t = (ave1 - ave2) / sqrt(var1/n1 + var2/n2)
+      ! Student's t
+      t = (ave1 - ave2) / sqrt(var1/n1 + var2/n2)
 
-   df = (var1/n1 + var2/n2)**2 / ((var1/n1)**2 / (n1 - 1) + (var2/n2)**2 / (n2 - 1))
-   ! significance
-   prob = betai(0.5d0 * df, 0.5d0, df/(df + t**2))
-end subroutine tutest
+      df = (var1/n1 + var2/n2)**2 / ((var1/n1)**2 / (n1 - 1) + (var2/n2)**2 / (n2 - 1))
+      ! significance
+      prob = betai(0.5d0 * df, 0.5d0, df/(df + t**2))
+   end subroutine tutest
 
-
-program main
-   ! COMPILATION:
-   ! gfortran -c betacf.f90 gammaln.f90 betai.f90 tutest.f90
-   ! gfortran betacf.o gammaln.o betai.o tutest.o -o fmain_tutest
-   ! EXECUTION:
-   ! ./fmain_tutest
-   
-   implicit none
-   integer                              :: n1, n2, i, c1, c2, cr
-   real(8), dimension(:), allocatable   :: data1, data2
-   real(8)                              :: t, prob, time_exec
-                                        
-
-   ! input data
-   data1 = (/1.0d0, 2.0d0, 3.0d0, 4.0d0, &
-             5.0d0, 6.0d0, 7.0d0, 8.0d0, 9.0d0/)
-   n1 = size(data1)
-
-   data2 = (/1.1d0, 2.0d0, 3.0d0, 4.0d0, &
-             5.0d0, 6.0d0, 7.0d0, 8.0d0, 9.0d0/)
-   n2 = size(data2)
-
-   ! timing of the for loop
-   call system_clock(count_rate=cr)
-   call system_clock(c1)
-   
-   do i = 1, 1000000
-      call tutest(data1, n1, data2, n2, t, prob)
-   end do
-
-   call system_clock(c2)
-   
-   time_exec = (c2 - c1) / real(cr)
-
-   write(*,*) t, prob, time_exec
-end program main
+end module mod_tutest
---
+++ test/Ch13_StatisticalDescriptionOfData/student_ttest/different_variances/test_tutest_f90.py
@@ -0,0 +1,86 @@
+"""
+    Python interface for the unit tests of the
+    tutest Fortran function.
+
+    Expected values obtained from SciPy
+"""
+import pytest
+from re import findall
+from os import system, chdir, getcwd
+from subprocess import run
+from scipy.stats import ttest_ind
+from numpy.testing import assert_almost_equal
+
+
+# Compile the Fortran tests source code to object files
+path2Ch13 = '../../../../src/numericalrecipes/Ch13_StatisticalDescriptionOfData/'
+path2Ch6 = '../../../../src/numericalrecipes/Ch6_SpecialFunctions/'
+path2fortran_test = 'test/Ch13_StatisticalDescriptionOfData/student_ttest/different_variances'
+
+owd = getcwd()
+chdir(path2fortran_test)  # chdir is needed due to pytest working from root dir
+system('gfortran -c ' + path2Ch6 + 'betacf/betacf.f90')
+system('gfortran -c ' + path2Ch6 + 'gammaln/gammaln.f90')
+system('gfortran -c ' + path2Ch6 + 'betai/betai.f90')
+system('gfortran -c ' + path2Ch13 + 'avevar/avevar.f90')
+system('gfortran -c ' + path2Ch13 + 'student_ttest/different_variances/tutest.f90')
+system('gfortran -c test_tutest.f90')
+system('gfortran avevar.o betai.o betacf.o gammaln.o \
+        tutest.o test_tutest.o -o ftest_tutest')  # Link objects
+system('rm *.o *.mod')  # Remove unnecessary objects and module
+chdir(owd)
+
+
+# Fortran wrapper function
+def fortran_tutest(data1, data2):
+    aux = [str(len(data1))]
+    aux += [str('') + str(s) + str('') for s in data1]
+    aux.insert(0, owd + '/' + path2fortran_test + '/ftest_tutest')
+
+    aux_ = [str(len(data2))]
+    aux_ += [str('') + str(s) + str('') for s in data2]
+    aux += aux_
+
+    result = run(aux, capture_output=True)
+    list_ = findall(r"\d+\.\d+", result.stdout.decode("utf-8"))
+    return [float(i) for i in list_]  # returns student's t and significance
+
+
+def test_tutest_means_0():
+    """
+        Checks against SciPy function
+    """
+    data1 = [1, 22, 3, 4, 5, 16, 7, 8, 9]
+    data2 = [1.1, 2, 3, 4, 5, 6, 7, 18, 9]
+
+    # Student's "t"
+    calculated_value = fortran_tutest(data1, data2)[0]
+    expected_value = ttest_ind(data1, data2, equal_var=False)[0]
+
+    assert_almost_equal(calculated_value, expected_value, 1e-6)
+
+    # Student's "significance"
+    calculated_value = fortran_tutest(data1, data2)[1]
+    expected_value = ttest_ind(data1, data2, equal_var=False)[1]
+
+    assert_almost_equal(calculated_value, expected_value, 1e-6)
+
+
+def test_tutest_means_1():
+    """
+        Checks against SciPy function
+    """
+    data1 = [1, 22, 3, 4, 5, 16, 17, 18, 99]
+    data2 = [1.1, 2, 3, 4, 5, 6, 7, 18, 9]
+
+    # Student's "t"
+    calculated_value = fortran_tutest(data1, data2)[0]
+    expected_value = ttest_ind(data1, data2, equal_var=False)[0]
+
+    assert_almost_equal(calculated_value, expected_value, 1e-6)
+
+    # Student's "significance"
+    calculated_value = fortran_tutest(data1, data2)[1]
+    expected_value = ttest_ind(data1, data2, equal_var=False)[1]
+
+    assert_almost_equal(calculated_value, expected_value, 1e-6)
