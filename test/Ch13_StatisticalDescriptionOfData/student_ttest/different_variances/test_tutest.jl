ea2aac334a2762a6c0097531d3687435850cfaa5
aqreed <andres.quezada.reed@gmail.com>
2019-11-04 23:26:16 +0100

    [JULIA] added test for "tuest" function.

---
+++ test/Ch13_StatisticalDescriptionOfData/student_ttest/different_variances/test_tutest.jl
@@ -0,0 +1,48 @@
+#=
+    Unit tests of the tutest function
+
+    Expected values obtained from HypothesisTests package
+=#
+using HypothesisTests
+
+
+@testset "test_tutest_means_0" begin
+    #=
+        Checks against HypothesisTests
+    =#
+    data1 = [1, 22, 3, 4, 5, 16, 7, 8, 9]
+    data2 = [1.1, 2, 3, 4, 5, 6, 7, 18, 9]
+
+    # Student's "t"
+    calculated_value = tutest(data1, data2)[1]
+    expected_value = UnequalVarianceTTest(data1, data2).t
+
+    @test isapprox(calculated_value, expected_value; atol=1e-6)
+
+    # Student's "significance"
+    calculated_value = tutest(data1, data2)[2]
+    expected_value = pvalue(UnequalVarianceTTest(data1, data2))
+
+    @test isapprox(calculated_value, expected_value; atol=1e-6)
+end
+
+
+@testset "test_tutest_means_1" begin
+    #=
+        Checks against HypothesisTests
+    =#
+    data1 = [1, 22, 3, 4, 5, 16]
+    data2 = [1.1, 2, 3, 4, 5, 6, 7, 18, 9]
+    
+    # Student's "t"
+    calculated_value = tutest(data1, data2)[1]
+    expected_value = UnequalVarianceTTest(data1, data2).t
+
+    @test isapprox(calculated_value, expected_value; atol=1e-6)
+
+    # Student's "significance"
+    calculated_value = tutest(data1, data2)[2]
+    expected_value = pvalue(UnequalVarianceTTest(data1, data2))
+
+    @test isapprox(calculated_value, expected_value; atol=1e-6)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,4 @@ using NumericalRecipes
 @testset "test_gammaln" begin include("Ch6_SpecialFunctions/gammaln/test_gammaln.jl") end
 @testset "test_avevar" begin include("Ch13_StatisticalDescriptionOfData/avevar/test_avevar.jl") end
 @testset "test_ttest" begin include("Ch13_StatisticalDescriptionOfData/student_ttest/different_means/test_ttest.jl") end
+@testset "test_tutest" begin include("Ch13_StatisticalDescriptionOfData/student_ttest/different_variances/test_tutest.jl") end
