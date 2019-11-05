6196220989b907a8813e4b3ac8641adac7e7f61b
aqreed <andres.quezada.reed@gmail.com>
2019-11-05 17:52:20 +0100

    [FORTRAN] added test for "tptest" function. Modified "tptest" file to contain only the subroutine

--- src/numericalrecipes/Ch13_StatisticalDescriptionOfData/student_ttest/paired_samples/tptest.f90
+++ src/numericalrecipes/Ch13_StatisticalDescriptionOfData/student_ttest/paired_samples/tptest.f90
@@ -1,76 +1,46 @@
-subroutine tptest(data1, n1, data2, n2, t, prob)
-   ! Given the arrays "data1" and "data2" of length "n", returns
-   ! the Student's "t" for paired data and its significance as "prob".
-   ! Small values of "prob" indicates that the arrays have different
-   ! means.
-   
-   use mod_betai
-   use mod_avevar
+module mod_tptest
+   ! Contains the following subroutine: tptest
    implicit none
-   real(8), intent(in)                  :: data1(n1), data2(n2)
-   integer, intent(in)                  :: n1, n2
-   integer                              :: n, i
-   real(8)                              :: ave1, var1, ave2, var2, &
-                                           df, t, prob, cov
+   contains
+
+   subroutine tptest(data1, n1, data2, n2, t, prob)
+      ! Given the arrays "data1" and "data2" of length "n", returns
+      ! the Student's "t" for paired data and its significance as "prob".
+      ! Small values of "prob" indicates that the arrays have different
+      ! means.
+      use mod_betai
+      use mod_avevar
+      implicit none
+      real(8), intent(in)                  :: data1(n1), data2(n2)
+      integer, intent(in)                  :: n1, n2
+      integer                              :: n, i
+      real(8)                              :: ave1, var1, ave2, var2, &
+                                              df, t, prob, cov
 
 
-   if (n1 /= n2) then
-      write(*, *) "Data arrays must have the same size"
-      stop
-   else
-      n = n1
-   end if
+      if (n1 /= n2) then
+         write(*, *) "Data arrays must have the same size"
+         stop
+      else
+         n = n1
+      end if
 
-   call avevar(data1, n1, ave1, var1)
-   call avevar(data2, n2, ave2, var2)
-    
-   cov = 0.0d0
-   do i = 1, n
-      cov = cov + (data1(i) - ave1) * (data2(i) - ave2)
-   end do
+      call avevar(data1, n1, ave1, var1)
+      call avevar(data2, n2, ave2, var2)
 
-   df = n - 1.0d0
-   cov = cov / df
-   
-   ! Student's t
-   t = (ave1 - ave2) / sqrt((var1 + var2 - 2.0d0 * cov) / n)
+      cov = 0.0d0
+      do i = 1, n
+         cov = cov + (data1(i) - ave1) * (data2(i) - ave2)
+      end do
 
-   ! significance
-   prob = betai(0.5d0 * df, 0.5d0, (df/(df + t**2)))
-end subroutine tptest
+      df = n - 1.0d0
+      cov = cov / df
 
+      ! Student's t
+      t = (ave1 - ave2) / sqrt((var1 + var2 - 2.0d0 * cov) / n)
 
-program main
-   ! COMPILATION:
-   ! gfortran -c betacf.f90 gammaln.f90 betai.f90 tptest.f90
-   ! gfortran betacf.o gammaln.o betai.o tptest.o -o fmain_tptest
-   ! EXECUTION:
-   ! ./fmain_tptest
+      ! significance
+      prob = betai(0.5d0 * df, 0.5d0, (df/(df + t**2)))
+   end subroutine tptest
 
-   implicit none
-   integer                              :: n1, n2, i, c1, c2, cr
-   real(8), dimension(:), allocatable   :: data1, data2
-   real(8)                              :: t, prob, time_exec
-
-
-   ! input data
-   data1 = (/70.0d0, 71.0d0, 72.0d0, 73.0d0, 70.5d0/)
-   n1 = size(data1)
-
-   data2 = (/70.15d0, 71.05d0, 71.95d0, 72.85d0, 70.45d0/)
-   n2 = size(data2)
-
-   ! timing of the for loop
-   call system_clock(count_rate=cr)
-   call system_clock(c1)
-
-   do i = 1, 1000000
-      call tptest(data1, n1, data2, n2, t, prob)
-   end do
-
-   call system_clock(c2)
-   
-   time_exec = (c2 - c1) / real(cr)
-
-   write(*,*) t, prob, time_exec
-end program main
+end module mod_tptest
---
+++ test/Ch13_StatisticalDescriptionOfData/student_ttest/paired_samples/test_tptest_f90.py
@@ -0,0 +1,86 @@
+"""
+    Python interface for the unit tests of the "tptest" Fortran function.
+    Contains the wrapper function and the tests.
+
+    Expected values obtained from SciPy
+"""
+import pytest
+from re import findall
+from os import system, chdir, getcwd
+from subprocess import run
+from scipy.stats import ttest_rel
+from numpy.testing import assert_almost_equal
+
+
+# Compile the Fortran tests source code to object files
+path2Ch13 = '../../../../src/numericalrecipes/Ch13_StatisticalDescriptionOfData/'
+path2Ch6 = '../../../../src/numericalrecipes/Ch6_SpecialFunctions/'
+path2fortran_test = 'test/Ch13_StatisticalDescriptionOfData/student_ttest/paired_samples/'
+
+owd = getcwd()
+chdir(path2fortran_test)  # chdir is needed due to pytest working from root dir
+system('gfortran -c ' + path2Ch6 + 'betacf/betacf.f90')
+system('gfortran -c ' + path2Ch6 + 'gammaln/gammaln.f90')
+system('gfortran -c ' + path2Ch6 + 'betai/betai.f90')
+system('gfortran -c ' + path2Ch13 + 'avevar/avevar.f90')
+system('gfortran -c ' + path2Ch13 + 'student_ttest/paired_samples/tptest.f90')
+system('gfortran -c test_tptest.f90')
+system('gfortran avevar.o betai.o betacf.o gammaln.o \
+        tptest.o test_tptest.o -o ftest_tptest')  # Link objects
+system('rm *.o *.mod')  # Remove unnecessary objects and module
+chdir(owd)
+
+
+# Fortran wrapper function
+def fortran_tptest(data1, data2):
+    aux = [str(len(data1))]
+    aux += [str('') + str(s) + str('') for s in data1]
+    aux.insert(0, owd + '/' + path2fortran_test + 'ftest_tptest')
+
+    aux_ = [str(len(data2))]
+    aux_ += [str('') + str(s) + str('') for s in data2]
+    aux += aux_
+
+    result = run(aux, capture_output=True)
+    list_ = findall(r"\d+\.\d+", result.stdout.decode("utf-8"))
+    return [float(i) for i in list_]  # returns student's t and significance
+
+
+def test_tptest_0():
+    """
+        Checks against SciPy function
+    """
+    data1 = [1, 22, 3, 4, 5, 16, 7, 8, 9]
+    data2 = [1.1, 2, 3, 4, 5, 6, 7, 18, 9]
+
+    # Student's "t"
+    calculated_value = fortran_tptest(data1, data2)[0]
+    expected_value = ttest_rel(data1, data2)[0]
+
+    assert_almost_equal(calculated_value, expected_value, 4)
+
+    # Student's "significance"
+    calculated_value = fortran_tptest(data1, data2)[1]
+    expected_value = ttest_rel(data1, data2)[1]
+
+    assert_almost_equal(calculated_value, expected_value, 4)
+
+
+def test_tptest_1():
+    """
+        Checks against SciPy function
+    """
+    data1 = [1, 22, 3, 4, 5, 16, 17, 18, 99]
+    data2 = [1.1, 2, 3, 4, 5, 6, 7, 18, 9]
+
+    # Student's "t"
+    calculated_value = fortran_tptest(data1, data2)[0]
+    expected_value = ttest_rel(data1, data2)[0]
+
+    assert_almost_equal(calculated_value, expected_value, 4)
+
+    # Student's "significance"
+    calculated_value = fortran_tptest(data1, data2)[1]
+    expected_value = ttest_rel(data1, data2)[1]
+
+    assert_almost_equal(calculated_value, expected_value, 4)
