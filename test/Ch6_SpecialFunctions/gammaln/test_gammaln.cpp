9eb0ad7c3b5eefc453eeef6e7cc9f802eb28e6a7
aqreed <andres.quezada.reed@gmail.com>
2020-01-15 23:04:25 +0100

    [C++] added gammaln tests and python interface function

---
+++ test/Ch6_SpecialFunctions/gammaln/test_gammaln.cpp
@@ -0,0 +1,18 @@
+// COMPILATION:
+// g++ gammaln.cpp test_gammaln.cpp -o cpptest_gammaln
+#include <iostream>
+using namespace std;
+
+double gammaln(double x);
+
+
+int main(int argc, char** argv) {
+   /* Function that captures argument, typecasts it to float,
+      calculates gammaln and redirects it to stdout */
+
+   double y = stod(argv[1]);
+
+   cout << gammaln(y) << endl;
+
+   return 0; 
+}
